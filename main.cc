b4883ff734d37ba3909916163e2623076a375641
Shaun Chong <data-miner00@users.noreply.github.com>
2025-07-26 12:31:41 +0800

    feat: Learn new stuffs

--- main.cc
+++ main.cc
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <cstdlib>
 #include <string>
+#include <limits>
 #include "bubble_sort.h"
 
 typedef std::string text_t;
@@ -11,6 +12,88 @@ T getSelf(T value) {
     return value;
 }
 
+constexpr int get_value() {
+    return 42;
+}
+
+void play_with_limits() {
+    std::cout << "int min: " << std::numeric_limits<int>::min() << '\n';
+    std::cout << "int max: " << std::numeric_limits<int>::max() << '\n';
+    std::cout << "double min: " << std::numeric_limits<double>::min() << '\n';
+    std::cout << "double max: " << std::numeric_limits<double>::max() << '\n';
+}
+
+void get_array_size() {
+    int array[] = { 1, 2, 3, 4, 5 };
+    std::cout << "Size of array: " << sizeof(array) / sizeof(array[0]) << '\n';
+    std::cout << "Size of array: " << std::size(array) << '\n';
+}
+
+void print_cstring() {
+    char message[5] { 'H', 'i', '!', '\0' }; // because have 5 space, non-initialized character will automatically be '\0'
+    std::cout << message << '\n';
+}
+
+void playing_with_pointers() {
+    int value = 42;
+    int* ptr = &value;
+
+    std::cout << "Value: " << value << '\n';
+    std::cout << "Pointer: " << ptr << '\n';
+    std::cout << "Dereferenced pointer: " << *ptr << '\n';
+
+    // Pointer arithmetic
+    int arr[] = { 1, 2, 3, 4, 5 };
+    int* arr_ptr = arr;
+
+    for (int i = 0; i < 5; ++i) {
+        std::cout << *(arr_ptr + i) << ' ';
+    }
+    std::cout << '\n';
+
+    int int_var {43};
+    int *p_int_var { &int_var };
+
+    std::cout << "The value for p_int_var is: " << *p_int_var << '\n';
+
+    // If we use {} to initialize a pointer, it will be initialized to nullptr
+    int* p_nullptr {};
+
+    // Explicitly initializing a pointer to nullptr
+    int* p_explicit_nullptr { nullptr };
+}
+
+void allocate_value_on_heap() {
+    // Allocating an integer on the heap
+    int* heap_value = new int(42);
+    std::cout << "Value allocated on heap: " << *heap_value << '\n';
+
+    // Don't forget to free the memory
+    // Do not call delete multiple times!
+    delete heap_value;
+    heap_value = nullptr;
+}
+
+void dynamic_arrays() {
+    size_t size = 5;
+
+    int* dynamic_array = new int[size];
+    for (size_t i = 0; i < size; ++i) {
+        dynamic_array[i] = static_cast<int>(i * 10);
+    }
+
+    int* safe_dynamic_array { new(std::nothrow) int[size]{} }; // All initialized to 0
+    int* safe_initialized_dynamic_array { new(std::nothrow) int[size]{1, 2, 3, 4, 5} };
+
+    delete[] dynamic_array;
+    dynamic_array = nullptr;
+
+    delete[] safe_dynamic_array;
+    safe_dynamic_array = nullptr;
+
+    delete[] safe_initialized_dynamic_array;
+    safe_initialized_dynamic_array = nullptr;
+}
 
 int main(int argc, char** argv) {
     text_t hello = "hello, world";
@@ -31,7 +114,7 @@ int main(int argc, char** argv) {
     text_t* invalid = nullptr;
 
     // Heap alloc
-    int* persist = new int;
+    number_t* persist = new int;
     *persist = 33;
     delete persist;
 
@@ -43,6 +126,21 @@ int main(int argc, char** argv) {
 
     int brance_init(500);
     std::cout << brance_init << '\n';
+
+    constexpr int value = get_value();
+    std::cout << value << '\n';
+
+    play_with_limits();
+
+    for (unsigned int i{}; i < 3; ++i) {
+        std::cout << "Printing " << i << '\n';
+    }
+
+    get_array_size();
+    print_cstring();
+    playing_with_pointers();
+    allocate_value_on_heap();
+    dynamic_arrays();
 
     return 0;
 }
